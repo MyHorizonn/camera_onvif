ad3b905a2cc00826cbed4016e104b68130fed7ac
Nikita <infernalfonik@gmail.com>
2022-10-12 13:55:54 +0400

    focus test

--- app/camera_ptz/routes.py
+++ app/camera_ptz/routes.py
@@ -10,9 +10,86 @@ from onvif import ONVIFCamera, exceptions
 active = False
 moverequest = None
 
+
 @camera_ptz.route('/')
 def main():
     return render_template('index.html')
+
+
+@camera_ptz.route('/focus', methods=['POST'])
+def focus():
+    print('focus')
+    data = request.json
+    if data is None:
+        data = request.form.to_dict()
+    cam_info = data['data']
+    print(cam_info)
+    mycam = None
+    try:
+        mycam = ONVIFCamera(cam_info['cam_ip'], int(cam_info['port']), cam_info['username'], cam_info['password'])
+        print("camera connected")
+    except (exceptions.ONVIFError) as e:
+        print("connect error")
+    if mycam != None:
+        
+        imaging = mycam.create_imaging_service()
+        media = mycam.create_media_service()
+        video_sources = media.GetVideoSources()[0]
+        request = imaging.create_type('SetImagingSettings')
+
+        print(video_sources)
+
+        return jsonify({'msg': 'ok'}), 200
+    return jsonify({'msg': 'err'}), 404
+
+
+
+@camera_ptz.route('/zoom_up', methods=['POST'])
+def zoom_up():
+    print('zoom up')
+    data = request.json
+    if data is None:
+        data = request.form.to_dict()
+    cam_info = data['data']
+    print(cam_info)
+    mycam = None
+    try:
+        mycam = ONVIFCamera(cam_info['cam_ip'], int(cam_info['port']), cam_info['username'], cam_info['password'])
+        print("camera connected")
+    except (exceptions.ONVIFError) as e:
+        print("connect error")
+    if mycam != None:
+        ptz = mycam.create_ptz_service()
+        media = mycam.create_media_service()
+        media_profile = media.GetProfiles()[0]
+        cam_request = ptz.create_type('GetConfigurationOptions')
+        cam_request.ConfigurationToken = media_profile.PTZConfiguration.token
+        ptz_configuration_options = ptz.GetConfigurationOptions(cam_request)
+
+        global moverequest
+        moverequest = ptz.create_type('ContinuousMove')
+        moverequest.ProfileToken = media_profile.token
+        if moverequest.Velocity is None:
+            moverequest.Velocity = ptz.GetStatus({'ProfileToken': media_profile.token}).Position
+
+        print ('zooming up...')
+        moverequest.Velocity.PanTilt.space = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].URI
+        moverequest.Velocity.Zoom.space = ptz_configuration_options.Spaces.ContinuousZoomVelocitySpace[0].URI
+        moverequest.Velocity.PanTilt.x = 0
+        moverequest.Velocity.PanTilt.y = 0
+        moverequest.Velocity.Zoom.x = 1.0
+        
+        print(moverequest.Velocity.Zoom)
+
+        global active
+        if active:
+            ptz.Stop({'ProfileToken': moverequest.ProfileToken})
+        active = True
+        ptz.ContinuousMove(moverequest)
+
+        return jsonify({'msg': 'ok'}), 200
+    return jsonify({'msg': 'err'}), 404
+
 
 @camera_ptz.route('/zoom_down', methods=['POST'])
 def zoom_down():
@@ -50,52 +127,6 @@ def zoom_down():
         moverequest.Velocity.Zoom.x = -1.0
 
         print(moverequest)
-        
-        print(moverequest.Velocity.Zoom)
-
-        global active
-        if active:
-            ptz.Stop({'ProfileToken': moverequest.ProfileToken})
-        active = True
-        ptz.ContinuousMove(moverequest)
-
-        return jsonify({'msg': 'ok'}), 200
-    return jsonify({'msg': 'err'}), 404
-
-@camera_ptz.route('/zoom_up', methods=['POST'])
-def zoom_up():
-    print('zoom up')
-    data = request.json
-    if data is None:
-        data = request.form.to_dict()
-    cam_info = data['data']
-    print(cam_info)
-    mycam = None
-    try:
-        mycam = ONVIFCamera(cam_info['cam_ip'], int(cam_info['port']), cam_info['username'], cam_info['password'])
-        print("camera connected")
-    except (exceptions.ONVIFError) as e:
-        print("connect error")
-    if mycam != None:
-        ptz = mycam.create_ptz_service()
-        media = mycam.create_media_service()
-        media_profile = media.GetProfiles()[0]
-        cam_request = ptz.create_type('GetConfigurationOptions')
-        cam_request.ConfigurationToken = media_profile.PTZConfiguration.token
-        ptz_configuration_options = ptz.GetConfigurationOptions(cam_request)
-
-        global moverequest
-        moverequest = ptz.create_type('ContinuousMove')
-        moverequest.ProfileToken = media_profile.token
-        if moverequest.Velocity is None:
-            moverequest.Velocity = ptz.GetStatus({'ProfileToken': media_profile.token}).Position
-
-        print ('zooming up...')
-        moverequest.Velocity.PanTilt.space = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].URI
-        moverequest.Velocity.Zoom.space = ptz_configuration_options.Spaces.ContinuousZoomVelocitySpace[0].URI
-        moverequest.Velocity.PanTilt.x = 0
-        moverequest.Velocity.PanTilt.y = 0
-        moverequest.Velocity.Zoom.x = 1.0
         
         print(moverequest.Velocity.Zoom)
 
